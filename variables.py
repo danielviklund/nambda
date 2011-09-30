aa2d952d11b7f6f9d02f67cb534d300da996e1a5
Daniel Viklund <daniel@tervento.se>
2011-09-30 16:50:34 +0200

    Begun working on variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,7 @@
 """
-Will store variables as a hashtable, VARIABLE/DATA.
+Will store variables as a dictionary, VARIABLE/DATA. Maybe VARIABLE/DATA/TYPE
 """
+
+variables = {'var1': "hello world!"}
+
+print variables
