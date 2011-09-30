86eb8204b187ba5721f2fdad89e0e5ae370d22f1
Daniel Viklund <daniel@tervento.se>
2011-09-30 16:58:13 +0200

    I began to outline a lex() function in lexer.py

--- lexer.py
+++ lexer.py
@@ -3,3 +3,9 @@ To lex or not to lex - that is the question.
 
 Will break up the nambda source code into TOKENS and VALUES.
 """
+
+def lex(filepath):
+	# Open file.
+	file = open(filepath, 'r');
+	# Save contents of file
+	# Tokenize contents
